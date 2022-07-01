baa587500b51ca6c3346a9fb9857ac0c232e8de9
Holger Lamm <Holger.Lamm@flexoptix.net>
2022-07-01 10:13:34 +0200

    Add mockup config

--- Makefile
+++ Makefile
@@ -9,6 +9,7 @@ ASTS:= $(addprefix $(OUTDIR)/,$(SRC:.c=.ast))
 
 MOCK_SRC := mockup.c
 MASTER_MOCK := $(OUTDIR)/mockups.mockup
+MOCKUP_CONFIG:=$(OUTDIR)/mockup_config.h
 MOCK_OBJ := $(addprefix $(OUTDIR)/,$(MOCK_SRC:.c=.o))
 
 CC := clang
@@ -20,11 +21,12 @@ $(OUT): $(OBJ) $(MOCK_OBJ) | $(OUTDIR)
 	$(CC) -o $@ $^
 
 $(MOCK_OBJ): $(MOCK_SRC) $(MASTER_MOCK)
+$(MOCK_OBJ): CFLAGS += --include $(MOCKUP_CONFIG)
 $(MASTER_MOCK): $(ASTS)
 	# Here be the magic stuff
 	# Loop the list of input files into a response files, for the colleagues using crappy operating systems
 	$(file >$@.inputs,$^)
-	python3 hammock.py @$@.inputs --symbols c some_var -o $@
+	python3 hammock.py @$@.inputs --symbols c some_var -o $@ --config $(MOCKUP_CONFIG)
 
 $(OUTDIR):
 	mkdir -p $@
--- hammock.py
+++ hammock.py
@@ -1,24 +1,31 @@
 from argparse import ArgumentParser
+from pathlib import Path
 
 if __name__ == "__main__":
    arg = ArgumentParser(fromfile_prefix_chars='@')
    arg.add_argument('--symbols', '-s', help="Symbols to mock", required=True, nargs='+')
    arg.add_argument('--output', '-o', help="Output")
+   arg.add_argument('--config', '-c', help="Mockup config header")
    arg.add_argument('files', nargs='+')
    args = arg.parse_args()
 
    # Fake
-   with open(args.output, 'w') as f:
-      f.write("""
-      #ifdef MOCKUP_CODE
-      int some_var;
-      #endif 
-      #ifdef MOCKUP_ADDITIONAL_GLOBAL_VARIABLES
-      extern int c__return;
-      #endif
-      #ifdef MOCKUP_CODE
-      int c(void) { 
-      return c__return; 
-      }
-      #endif
-      """)
+   Path(args.output).write_text("""
+#ifdef HAM_some_var
+#ifdef MOCKUP_CODE
+int some_var;
+#endif 
+#endif // HAM_some_var
+
+#ifdef HAM_c
+#ifdef MOCKUP_ADDITIONAL_GLOBAL_VARIABLES
+extern int c__return;
+#endif
+#ifdef MOCKUP_CODE
+int c(void) { 
+return c__return; 
+}
+#endif
+#endif // HAM_c
+""")
+   Path(args.config).write_text('\n'.join("#define HAM_%s" % symbol for symbol in args.symbols))
