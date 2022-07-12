4464f9a36ef77ccde5d605925f96b7b37223de67
Karsten Guenther <karsten.guenther@marquardt.com>
2022-07-12 17:51:55 +0200

    Fix actions, funny: Linux fails because Ninja is missing

--- Test/mini_c_test.py
+++ Test/mini_c_test.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import os
+from platform import system
 from Test.utils import run_process
 
 
@@ -8,6 +9,10 @@ class TestMiniCProject:
     def test_clean_build(self):
         project_dir = "resources/mini_c"
         build_dir = f"{project_dir}/build"
+        if system() is "Windows":
+            binary = f"{build_dir}/mini_c.exe"
+        else:
+            binary = f"{build_dir}/mini_c"
 
         exit_code = run_process(["cmake", "-S", project_dir, "-B", build_dir, "-G", "Ninja"])
         """CMake configure shall be successful."""
@@ -17,14 +22,14 @@ class TestMiniCProject:
         """CMake clean shall be successful."""
         assert exit_code == 0
         assert not os.path.isfile(f"{build_dir}/mockups.mockup")
-        assert not os.path.isfile(f"{build_dir}/mini_c.exe")
+        assert not os.path.isfile(binary)
 
         exit_code = run_process(["cmake", "--build", build_dir])
         """CMake build shall create expected mockups and binary."""
         assert exit_code == 0
         assert os.path.isfile(f"{build_dir}/mockups.mockup")
-        assert os.path.isfile(f"{build_dir}/mini_c.exe")
+        assert os.path.isfile(binary)
 
-        exit_code = run_process([f"{build_dir}/mini_c.exe"])
+        exit_code = run_process([f"./{binary}"])
         """Built binary shall be executable"""
         assert exit_code == 0
